f1af767f59e8376a63dce74f21927fc936089a37
Yeren Palacios <yeren.palacios@omnilatam.com>
2022-01-08 18:19:30 -0500

    modified login

--- instagram_app/views/user.py
+++ instagram_app/views/user.py
@@ -1,15 +1,25 @@
 from rest_framework.response import Response
-from rest_framework.generics import  ListAPIView, GenericAPIView, ListCreateAPIView
+from rest_framework.generics import  ListAPIView, GenericAPIView, ListCreateAPIView, CreateAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.models import Token
 
 from instagram_app.models import User
 from instagram_app.serializers import LoginSerializer, UserSerializer, ProfileStoriesSerializer
+from instagram_app.serializers.user import UserSignUpSerializer
 
 
 class UserView(ListCreateAPIView):
     serializer_class = UserSerializer
     queryset = serializer_class.Meta.model.objects.all()
+
+
+class UserDetailView(RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = serializer_class.Meta.model.objects.all()
+
+
+class UserSignupView(CreateAPIView):
+    serializer_class = UserSignUpSerializer
 
 
 class LoginView(GenericAPIView):
@@ -21,7 +31,7 @@ class LoginView(GenericAPIView):
         serializer.is_valid(raise_exception=True)
         user, token = serializer.save()
         data = {
-            'user': self.serializer_class(user).data,
+            'user': UserSerializer(user).data,
             'token': token
         }
         return Response(data,status=201)
@@ -41,5 +51,9 @@ class LogoutView(GenericAPIView):
 
 
 class ProfileStoriesView(ListAPIView):
+    permission_classes = (IsAuthenticated,)
     serializer_class = ProfileStoriesSerializer
-    queryset = User.objects.all()
+    def get_queryset(self):
+        print(self.request.user.id)
+        qs = User.objects.exclude(id=self.request.user.id)[:7]
+        return qs
