dc75c6a3bf5056caaec2d2080ff89438697c3006
Yeren Palacios <yerenagmt@gmail.com>
2023-05-17 20:54:09 -0500

    reformat file and removed weird comments

--- instagram/settings.py
+++ instagram/settings.py
@@ -18,7 +18,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Quick-start development settings - unsuitable for production
@@ -37,7 +36,6 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 WHITENOISE_USE_FINDERS = True
-
 
 # Application definition
 
@@ -102,28 +100,22 @@ CHANNEL_LAYERS = {
     },
 }
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-		'ENGINE': 'django.db.backends.postgresql_psycopg2',
-		'NAME': 'instagram_db',
-		'USER' : 'postgres',
-		'PASSWORD' : '1234',
-		'HOST' : 'localhost', #si tienes otra dirección host debes remplazar esta
-		'PORT' : '5432', #si lo dejas vacío tomara el puerto por default
-	},
-    # 'test': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'instagram_db',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
 }
 
 STATIC_HOST = "https://dry-brook-25412.herokuapp.com/" if not DEBUG else ""
 STATIC_URL = os.path.join(STATIC_HOST + "/static/")
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -143,7 +135,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -157,11 +148,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
