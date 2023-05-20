5a3623afc77cc4bbaa9746f5f3763f808ded144e
Yeren Palacios <yeren.palacios@omnilatam.com>
2023-05-19 20:19:39 -0500

    added files for comment object

---
+++ instagram_app/repositories/comment_repository.py
@@ -0,0 +1,13 @@
+from django.db.models import QuerySet
+
+from instagram_app.models import Comment
+
+
+class CommentRepository:
+    @staticmethod
+    def get_comments_by_post(post_id: int) -> QuerySet[Comment]:
+        return Comment.objects.filter(post_id)
+
+    @staticmethod
+    def create_post_comment(post_id: int, user_id: int, content: str) -> Comment:
+        return Comment.objects.create(post_id=post_id, user_id=user_id, text=content)
---
+++ instagram_app/services/comment_service.py
@@ -0,0 +1,12 @@
+from instagram_app.repositories.comment_repository import CommentRepository
+from instagram_app.serializers import CommentViewSerializer
+
+
+class CommentService:
+
+    def __init__(self):
+        self._repository = CommentRepository()
+
+    def get_post_comments(self, post_id: int):
+        comments = self._repository.get_comments_by_post(post_id)
+        return CommentViewSerializer(comments, many=True)
