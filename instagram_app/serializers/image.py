f867462a5dc8ad86c3d85284cf4879c826f7449c
Yeren Palacios <yerenagmt@gmail.com>
2023-06-25 15:46:29 -0500

    added to generate a thumbnail for videos

--- instagram_app/serializers/image.py
+++ instagram_app/serializers/image.py
@@ -3,7 +3,8 @@ from rest_framework import serializers
 from instagram_app.models import Files
 
 class FilesSerializer(serializers.ModelSerializer):
+    thumbnail = serializers.CharField(required=False)
 
     class Meta:
         model = Files
-        fields = ['file']
+        fields = ['file', 'thumbnail']
