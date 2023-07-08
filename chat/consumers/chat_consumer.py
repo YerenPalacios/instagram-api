1f591825f662ad38d60d249f35666a587a864759
Yeren Palacios <yerenagmt@gmail.com>
2023-07-08 18:49:30 -0500

    moved chat comsumer to folder

--- chat/consumers/chat_consumer.py
+++ chat/consumers/chat_consumer.py
@@ -1,78 +1,17 @@
-# chat/consumers.py
-import json
 import logging
 
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import JsonWebsocketConsumer, WebsocketConsumer
+from channels.generic.websocket import JsonWebsocketConsumer
 
+from chat.models import ChatRoom
 from chat.services.chat_service import ChatService
-from instagram_app.serializers.message import MessageSerializer, ChatRoomMessageSerializer
-from chat.models import *
 from instagram_app.services.user_service import UserService
+
 
 logging.basicConfig(
     format="%(asctime)s %(message)s",
     level=logging.INFO,
 )
-
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
-
-        # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
-            self.channel_name
-        )
-
-        self.accept()
-
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name,
-            self.channel_name
-        )
-
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
-
-    # Receive message from room group
-    def chat_message(self, event):
-        message = event['message']
-
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
-
-
-def get_messages(user, send_to):
-    # qs = MessageSerializer.Meta.model.objects.filter(
-    #         Q(user=user) & Q(send_to=send_to) |
-    #         Q(user=send_to) & Q(send_to=user)
-    #     ).order_by('created_at')
-    return qs
-
-def save_message(msg, by_user):
-    model = MessageSerializer.Meta.model
-    a = model.objects.create(
-        text=msg['text'],
-        user_id=by_user.id,
-        send_to_id=msg['send_to']
-    )
-    message = MessageSerializer(a).data
-    return message
 
 
 class ChatConsumer2(JsonWebsocketConsumer):
